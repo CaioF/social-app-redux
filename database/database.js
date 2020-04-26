9a263b5d35749aaeefee2c5fdd965565e05d7bf3
Caio Fleury <caio.fleury.r@gmail.com>
2020-04-27 01:11:21 +0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,5 +1,5 @@
 const Sequelize     = require('sequelize');
-const sequelize     = new Sequelize('postgres://gjemaffzmyxkiy:cdcb5cb7571f8cdb8222ff6ed1e93505176e42d7df11bb4c2e3e95c6a09c258b@ec2-54-147-209-121.compute-1.amazonaws.com:5432/ddo0ojgvduccbb', {
+const sequelize     = new Sequelize('', {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
